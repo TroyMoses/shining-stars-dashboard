c605b7a5b32da26517ac3384534a6a39374b8d05
TroyMoses <mugabimoses07@gmail.com>
2024-07-21 01:54:39 +0800

    events news

--- shining-stars-client/src/App.tsx
+++ shining-stars-client/src/App.tsx
@@ -83,7 +83,7 @@ function App() {
       const profileObj = credential ? parseJwt(credential) : null;
 
       if (profileObj) {
-        const response = await fetch("https://shining-stars-dashboard.onrender.com/api/v1/users", {
+        const response = await fetch("http://localhost:8080/api/v1/users", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -151,7 +151,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("https://shining-stars-dashboard.onrender.com/api/v1")}
+          dataProvider={dataProvider("http://localhost:8080/api/v1")}
           notificationProvider={useNotificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
