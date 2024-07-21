58ef9f00902c15175ab99ba170ae96c08e944397
TroyMoses <mugabimoses07@gmail.com>
2024-07-21 15:20:34 +0800

    contact

--- shining-stars-server/mongodb/models/contact.js
+++ shining-stars-server/mongodb/models/contact.js
@@ -4,7 +4,6 @@ const ContactSchema = new mongoose.Schema({
   email: { type: String, required: true },
   subject: { type: String },
   message: { type: String, required: true },
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 const ContactModel = mongoose.model("Contact", ContactSchema);
